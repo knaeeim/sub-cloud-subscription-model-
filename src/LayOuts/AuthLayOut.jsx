fe72c40cc7628d1ae50495701f1afdd1b246229f
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2025-05-07 12:04:04 +0530

    Added Footer on Login and Register Page

--- src/LayOuts/AuthLayOut.jsx
+++ src/LayOuts/AuthLayOut.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
 
 const AuthLayOut = () => {
     return (
@@ -9,6 +10,7 @@ const AuthLayOut = () => {
             <div className="w-11/12 mx-auto min-h-[calc(100vh-100px)] flex items-center justify-center className='bg-[#90C2E7]'">
                 <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
